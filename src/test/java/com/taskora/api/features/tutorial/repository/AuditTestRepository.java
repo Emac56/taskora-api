46d5da92a705b40dcfea284a97b97e3074a6f7e9
Emac56 <emacvicente@gmail.com>
2026-08-05 22:47:19 +0800

    feat: add base entity and jpa auditing support

--- src/test/java/com/taskora/api/features/tutorial/repository/AuditTestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taskora.api.features.tutorial.repository;
-
-import com.taskora.api.features.tutorial.entity.AuditTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditTestRepository extends JpaRepository<AuditTest, Long> {
-}
