5bad949a53a90c29cf800b0f3034cabc9e60bf8a
Emac56 <emacvicente@gmail.com>
2026-08-06 12:10:48 +0800

    tutorial(entity): create an tutorial entity

---
+++ src/main/java/com/taskora/api/features/tutorial/entity/Tutorial.java
@@ -0,0 +1,27 @@
+package com.taskora.api.features.tutorial.entity;
+
+import com.taskora.api.common.entity.BaseEntity;
+import com.taskora.api.features.tutorial.enums.TutorialStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tutorials")
+public class Tutorial extends BaseEntity {
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TutorialStatus status;
+
+}
